682c24012b0ec0271b9ad0546d2cd195062a367a
John Bagley <jdbagley@gmail.com>
2014-04-04 10:09:40 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RedditClone::Application.config.secret_key_base = 'eee91c7dc21f04725d91d7e50c4ce292e222f03f6fdaf93c30c79d3888ab8e3b590ab00c4a31b777c0933446a0de4fe14c0590efddaf0c416e4854c5cd8a4251'
